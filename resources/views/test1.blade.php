cfae1f57eaef213bec296a702508c50fa338c2b4
samaralii <samarali@live.com>
2021-02-22 12:26:05 +0500

    initial commit

---
+++ resources/views/test1.blade.php
@@ -0,0 +1,4 @@
+<h1> Welcome to Test 1 Page </h1>
+
+{{ $foo }}
+{{ $haystack }}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test1', function () {
+    return view('test1', ['foo' => 'bar1', 'haystack' => 'needle']);
+});
+
+Route::get('/test2', function () {
+    return view('test2', ['foo' => 'bar2']);
+});
