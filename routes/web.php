945691b34a11e582e54abc9a0d314682a90b0fb3
samaralii <samarali@live.com>
2021-02-22 14:54:49 +0500

    fresh start

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\TestsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test1', [TestsController::class, 'one']);
-
-Route::get('/test2', [TestsController::class, 'two']);
-
